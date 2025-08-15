0b77fc465d4cd3d2e948c9bee09bfc8a6a3d53a4
vijayasai0021 <vijayasai0021@gmail.com>
2025-08-15 18:08:50 +0530

    remainig routes of events are completed

--- src/routes/events.js
+++ src/routes/events.js
@@ -13,4 +13,8 @@ router.get('/all',authMiddleware,authorizeRoles('ADMIN'),eventController.getAllE
 router.get('/:id',eventController.getEventById);
 router.get('/name/:name',eventController.getEventByName);
 router.put('/:id',authMiddleware,authorizeRoles('ADMIN','EVENT_CONDUCTOR'),eventController.updateEvent);
+router.post('/:id/register',authMiddleware,eventController.registerForEvent);
+router.get('/:id/registrations',authMiddleware,authorizeRoles('ADMIN','EVENT_CONDUCTOR'),eventController.getRegistrationsForEvent);
+router.delete('/:id/delete',authMiddleware,authorizeRoles('ADMIN','EVENT_CONDUCTOR'),eventController.deleteEvent);
+
 module.exports = router;
